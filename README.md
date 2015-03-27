b3c1ac1990eb0fa2ff18eb8967d5e89a4c9ec7b3
kyle roux <kyle@level2designs.com>
2015-03-27 02:32:35 -0700

    configuration through providers and constants

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 ## Still WIP, <small>to use you must download and install webdis, get it at [webd.is](webd.is)</small>
+
+###making progress
+
+to use, just add dependency to redis.app
+
+```javascript
+var myApp = angular.module('mycool.app',['redis.app']);
+```
+
+to use different host or port for webdis, use redisUrlProvider in config
+
+```javascript
+myApp.config(['redisUrlProvider',function(redisUrlProvider){
+    redisUrlProvider.set('host','myhost.com');
+    redisUrlProvider.set('port',4444);
+}]);
+```
--- redis.app.js
+++ redis.app.js
@@ -1,23 +1,73 @@
 'use strict';
 
-var forEach = angular.forEach;
+var forEach = angular.forEach,
+    equals = angular.equals,
+    extend = angular.extend,
+    fromJson = angular.fromJson,
+    isArray = angular.isArray,
+    isDefined = angular.isDefined;
 
-var redisApp = angular.module('redis.app',[]);// REQUIRES THE USE OF WEDBIS HTTP SERVER
+var app = angular.module('redis.app',[]);
+// use redisProvider to configure 
+app.constant('REDIS_HOST','127.0.0.1'); // standard webdis host
+app.constant('REDIS_PORT',7379); // standard webdis port
 
-redisApp.factory('REDIS_URL',[function REDIS_URL(){
-    return function(){
-        return "http://174.140.227.137:7379/";
+app.provider('redisUrl',['REDIS_HOST','REDIS_PORT',function(REDIS_HOST,REDIS_PORT){
+  /* 
+   * use redisUrlProvider.set('host',xxxx); 
+   * and redisUrlProvider.set('port',xxxx); 
+   * inside a config function
+   * to customize your setup
+   */
+    var self = this;
+
+    self.host = self.host ? self.host : REDIS_HOST;
+    self.port = self.port ? self.port : REDIS_PORT;
+    self.set = function(key,val){
+        self[key] = val;
+    };
+    return {
+        $get:[function(){
+            return "http://"+self.host+":"+self.port;
+        }],
+        set:self.set
     };
 }]);
-redisApp.factory('redisUrlService',['REDIS_URL',function redisUrlService(REDIS_URL){
-    return function(cmd,args){
-            var url = REDIS_URL()+cmd+'/';
-            if(args && args.length){
-                forEach(args,function(itm,idx){
-                        url = url + itm + (idx != args.length-1 ? '/' : '');                    
-                });
+
+app.factory('redisArgs',[function(){
+    return function(args){
+        var rtn = '';
+        if(!isArray(args)){
+            forEach(args,function(val,key){
+                rtn += '/'+key+'/'+val;
+            });
+        }else{
+            forEach(args,function(val){
+                rtn += '/'+val;
+            });
+        }
+        return rtn;
+    };
+}]);
+app.factory('redisRequest',['$http','redisUrl','redisArgs',function($http,redisUrl,redisArgs){
+    var url = redisUrl;
+    return function(reqType,args){
+        return $http.get(url+redisArgs([reqType])+redisArgs(args));
+    };
+}]);
+app.factory('redisGet',['redisRequest','$q',function(redisRequest,$q){
+    var reqType = 'get';
+    var promise = $q.defer();
+    return function(args){
+        redisRequest(reqType,args).then(function(res){
+            try {
+                var rtn = fromJson(res.data.get);
+            }catch(err){
+                var rtn = res.data.get;
             }
-            return url;
+            promise.resolve(rtn);
+        });
+        return promise.promise;
     };
 }]);
 redisApp.factory('redisCallService',['$http','redisUrlService',function redisCallService($http,redisUrlService){
@@ -27,7 +77,7 @@ redisApp.factory('redisCallService',['$http','redisUrlService',function redisCal
         );
     };
 }]);
-redisApp.service('redis',['redisCallService','$q',function redis(redisCallService,$q){
+redisApp.service('redisService',['redisCallService','$q',function redis(redisCallService,$q){
     var self = this;
 
     self.get = function(key){
